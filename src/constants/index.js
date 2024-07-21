3f69e1847700e6db6a67d20ad4edab5f0ffb6734
Rajat Patel <patelrajat394120@gmail.com>
2024-07-21 23:30:20 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a dedicated full stack developer specializing in the MERN stack, passionate about crafting robust and scalable web applications. I have developed several projects using front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. Currently, I am expanding my skill set to include data analysis. I am actively seeking an internship to apply my knowledge, contribute to innovative solutions, and enhance my learning through real-world projects. My goal is to leverage my expertise to drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. Specializing in the MERN stack, I have developed several projects using technologies such as React, Next.js, Node.js, MySQL, and MongoDB. My journey in web development began with a deep curiosity about how things work, and it has grown into a career where I continuously seek to learn and embrace new challenges. I thrive in collaborative environments and enjoy tackling complex problems to deliver high-quality solutions. Currently, I am expanding my skill set to include data analysis. Beyond coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Web Developer Intern ",
+    company: "Encryptix",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB", "HTML", "CSS", "Node.js"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Airbnb Clone(WanderLust)",
+    image: project1,
+    description:
+      "I developed an Airbnb clone using the MERN stack, featuring user authentication, property listings with descriptions and photos, a dynamic search and filter system, a comprehensive booking system, and user reviews and ratings. The application is responsive and optimized for both desktop and mobile users, with a robust backend API built using Node.js and Express, and efficient data management with MongoDB. This project demonstrates my ability to create scalable, user-friendly web applications.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "To-do(Task Manager)",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Material UI", "TailWindCss"],
+  },
+  {
+    title: "Spotify Clone Page",
+    image: project4,
+    description:
+      "I developed a Spotify clone webpage using HTML, CSS, and JavaScript. This project replicates the design and functionality of the popular music streaming platform, featuring a responsive user interface with music library browsing, custom player controls, and seamless navigation. It showcases my proficiency in front-end development, emphasizing user engagement and interactive design principles.",
+    technologies: ["HTML", "CSS", "mySQL", "Js"],
+  },
+];
+
+export const CONTACT = {
+  address: "",
+  phoneNo: "",
+  email: "",
+};
